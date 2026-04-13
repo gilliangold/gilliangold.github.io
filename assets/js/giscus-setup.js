174f973be727d53c48ec87475f410267958ec9cd
gilliangold <36736897+gilliangold@users.noreply.github.com>
2026-04-13 16:51:01 +0000

    Deploying to gh-pages from @ gilliangold/gilliangold.github.io@ed0deba6ae6c59d0cb1885e2d3d31b65407f9afb 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "gilliangold/gilliangold",
+    "data-repo": "gilliangold/gilliangold.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
